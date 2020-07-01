fdb86cffa38f64af618b7b0ca48572d10c74126c
Vimukthi Sineth <vimukthisineth@gmail.com>
2020-07-01 19:52:43 +0530

    Init

---
+++ src/test/java/com/sliit/chat/bot/ChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.chat.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
